2940987de5b65f8999962cbe50a874296c540aae
hitenzo <wmowski@gmail.com>
2017-07-10 01:49:04 +0200

    init project

---
+++ ImagesViewer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImagesViewer.Startup))]
+namespace ImagesViewer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
